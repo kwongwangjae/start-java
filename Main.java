8e7829263266299573bc390ea3458a4f9865aff8
kwongwangjae <kgj9906@naver.com>
2024-01-03 17:51:52 +0900

    feat: 별문제

--- Main.java
+++ Main.java
@@ -1,36 +1,41 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-	public String solution(String str){
+	public String solution(int n){
 		String answer = "";
-		char[] s = str.toCharArray();
-		int lt = 0, rt = s.length-1;
-		while (lt < rt){
-			char tmp = s[lt];
-			if(!Character.isAlphabetic(s[lt]))lt++;
-			else if (!Character.isAlphabetic(s[rt]))rt--;
-			else {
-				s[lt] = s[rt];
-				s[rt] = tmp;
-				lt++;
-				rt--;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				answer += " ";
 			}
+			for(int k = 0; k < (2 * n -1) - (2 * i); k++){
+				answer += "*";
+			}
+			answer += "\n";
 		}
-		String tmp = String.valueOf(s);
-		answer = tmp;
+
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n - i; j++){
+				answer += " ";
+			}
+			for(int k = 0; k < i * 2 + 1; k++){
+				answer += "*";
+			}
+			answer += "\n";
+		}
 
 
-		return  answer;
+
+		return answer;
+
+
 	}
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		String str = kb.nextLine();
-		System.out.println(T.solution(str));
-		}
-
+		int n = kb.nextInt();
+		System.out.print(T.solution(n));
+	}
 }
 
 
