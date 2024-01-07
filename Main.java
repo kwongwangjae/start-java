49ba5caa0533eae8940479745751b2bb711893c2
kwongwangjae <kgj9906@naver.com>
2024-01-07 15:28:57 +0900

    feat: 팰린드롬 코드

--- Main.java
+++ Main.java
@@ -2,30 +2,46 @@ import java.util.Scanner;
 
 public class Main {
 
-	public int solution(String n){
-		int answer = 1;
-		n = n.toLowerCase();
-		StringBuilder sn = new StringBuilder(n);
-		sn.reverse();
-		for(int i = 0; i < n.length(); i++){
-			if(n.charAt(i) != sn.charAt(i)){
-				answer = 0;
-			}
-		}
+	// public boolean solution(String s){
+	// 	int start = 0;
+	// 	int end = s.length() - 1;
+	// 	//서로 중앙으로 이동해 나가다 곂치는 지점에 도달하면 종료
+	// 	while (start < end){
+	// 		//영숫자인지 판별하고 유효하지 않으면 한 칸 씩 이동
+	// 		if(!Character.isLetterOrDigit(s.charAt(start))){
+	// 			start++;
+	// 		} else if (!Character.isLetterOrDigit(s.charAt(end))) {
+	// 			end--;
+	// 		} else{ // 유효한 문자라면 앞 글자와 뒷글자를 모두 소문자로 변경해서 비교
+	// 			if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))){
+	// 				return false;
+	// 			}
+	// 			start++;
+	// 			end--;
+	// 		}
+	// 	}
+	// 	//무사히 실행되면 true 반환
+	// 	return true;
+	//
+	// }
 
-		return answer;
-		
+	// 이코드의 특징은 굉장히 정석적임 그럴 수 밖에 없는게 전체 다 돌지 않고도 처리를 할 수 있음
+
+	public boolean solution(String s){
+		String s_filter = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+		String s_reversed = new StringBuilder(s_filter).reverse().toString();
+
+		return s_filter.equals(s_reversed);
+
 	}
+	//
 	public static void main(String[] args) {
 		Main T = new Main();
 		Scanner kb = new Scanner(System.in);
-		String n = kb.next();
-		System.out.print(T.solution(n));
+		String s = kb.next();
+		System.out.print(T.solution(s));
 	}
 }
-
-
-//생각한 팰린드롬
 
 
 
